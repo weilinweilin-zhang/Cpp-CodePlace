8e3689c2ce410d70227ab30376a463a070202d9e
张伟林 <zwl1@meitu.com>
2023-11-02 18:45:56 +0800

    单例模式完善

---
+++ DesignerModes/Singleton/main.cpp
@@ -0,0 +1,13 @@
+#include "Singleton.h"
+
+int main()
+{
+    std::cout <<"If you see the same value, then singleton was reused (yay!\n" <<
+                "If you see different values, then 2 singletons were created (booo!!)\n\n" <<
+                "RESULT:\n";   
+    std::thread t1(ThreadFoo);
+    std::thread t2(ThreadBar);
+    t1.join();
+    t2.join();
+    return 0;
+}
